1b1c1e372b8217ba926941ec8dfa1545da676dd1
Conaill Hyndman <chyndman@deloitte.co.uk>
2019-01-08 15:52:38 +0000

    continuing to checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,28 +91,29 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Conaill Hyndman',
-                address: {
-                    street: '1 High Street',
-                    zipCode: 'BT44 0HD',
-                    country: 'Ireland'
-                },
-                email: 'test@example.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
+    //     this.setState({ loading: true });
+    //     const order = {
+    //         ingredients: this.state.ingredients,
+    //         price: this.state.totalPrice,
+    //         customer: {
+    //             name: 'Conaill Hyndman',
+    //             address: {
+    //                 street: '1 High Street',
+    //                 zipCode: 'BT44 0HD',
+    //                 country: 'Ireland'
+    //             },
+    //             email: 'test@example.com'
+    //         },
+    //         deliveryMethod: 'fastest'
+    //     }
+    //     axios.post('/orders.json', order)
+    //         .then(response => {
+    //             this.setState({ loading: false, purchasing: false });
+    //         })
+    //         .catch(error => {
+    //             this.setState({ loading: false, purchasing: false });
+    //         });
+    this.props.history.push('/checkout');
     }
 
     render() {
